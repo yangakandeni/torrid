fb27951bbb7da27961c0809f3b08a80ea1a74e8c
yangakandeni <kandeni.yanga@gmail.com>
2021-04-28 00:33:31 +0200

    implement user registration

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import PasswordChangeDoneView, PasswordChangeView, PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetView
 
-from .views import dashboard
+from .views import dashboard, register
 
 # app_name = "account"
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("register/", register, name="register"),
     path("password/change/", PasswordChangeView.as_view(), name="password_change"),
     path("password/change/success/", PasswordChangeDoneView.as_view(), name="password_change_done"),
     path('password/reset/', PasswordResetView.as_view(), name='password_reset'),
