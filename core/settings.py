0341343ab5266e56bbd8d4b26fdeb4dbd2c5cc2d
yangakandeni <kandeni.yanga@gmail.com>
2021-04-27 20:32:34 +0200

    add account app

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # own
+    'account.apps.AccountConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
